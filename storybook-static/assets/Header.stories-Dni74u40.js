9bf250f4949e15360a34b63c2a5cc2e8d3284288
zongzhenjiang <zongzhenjiang51@gmail.com>
2025-04-08 19:48:45 +0100

    commit

---
+++ storybook-static/assets/Header.stories-Dni74u40.js
@@ -0,0 +1,7 @@
+import{f as o}from"./index-BZkcKs8Z.js";import{H as d}from"./Header-C3vupnj-.js";import"./index-D4lIrffr.js";import"./Button--hR7c5W6.js";const l={title:"Example/Header",component:d,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const f=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,f as __namedExportsOrder,l as default};
